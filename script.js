db7a1f3fbc88aff6f43f06cd38813b86632b9729
Abdulrazak Idris <Abdulrazakidrisyahaya@gmail.com>
2024-01-31 15:19:39 +0100

    Implement refresh action

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ let body = document.querySelector("body");
 container.classList.add("container");
 body.appendChild(container);
 
-// Flex items.
-for (let i = 0; i < 200; i++) {
-  let gridItem = document.createElement("div");
-  gridItem.classList.add("flex-item");
-  container.append(gridItem);
-}
-
-let gridItem = document.querySelectorAll(".flex-item");
-gridItem.forEach((item) => {
-  item.addEventListener("mouseover", function () {
-    item.classList.add("touchColor");
-  });
+// Button.
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", function () {
+  container.textContent = null;
+  let numOfgrids;
+  let number = +prompt("Number of grids!!");
+  if (number > 100 || number < 0) {
+    alert("You've entered the wrong input!!");
+  } else {
+    numOfgrids = number;
+    // Number of grids.
+    for (let i = 1; i <= numOfgrids; i++) {
+      let gridItem = document.createElement("div");
+      gridItem.classList.add("flex-item");
+      container.append(gridItem);
+    }
+    let gridItem = document.querySelectorAll(".flex-item");
+    gridItem.forEach((item) => {
+      item.addEventListener("mouseover", function () {
+        item.classList.add("touchColor");
+      });
+    });
+  }
 });
